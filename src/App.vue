0c99fe3ce71fb1b9f24bf6e9d3119ecd87666a7c
Cindy <cindylay524@gmail.com>
2018-07-12 17:25:35 +0700

    fuckin done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+
+  <div id="app" class="container">
+    <div class="page-header">
+      <h1>This Application uses both Firebase and Vue!</h1>
+    </div>
+
+    <div class="card">
+      <div class="card-header">Add Book</div>
+      <div class="card-body">
+        <form id="form"
+              class="form-inline"
+              @submit.prevent="addArticle">
+          <div class="form-group mb-2">
+            <label for="title" class="sr-only">Title</label>
+            <input id="title"
+                  type="text"
+                  class="form-control"
+                  placeholder="Title"
+                  v-model="newArticle.title" />
+          </div>
+          <div class="form-group mx-sm-3 mb-2">
+            <label for="author" class="sr-only">Author</label>
+            <input id="author"
+                  type="text"
+                  class="form-control"
+                  placeholder="Author"
+                  v-model="newArticle.author" />
+          </div>
+          <div class="form-group mb-2">
+            <label for="url" class="sr-only">Url</label>
+            <input id="url"
+                  type="text"
+                  class="form-control"
+                  placeholder="http://"
+                  v-model="newArticle.url" />
+          </div>
+          <button class="btn btn-primary mx-sm-3 mb-2">Add</button>
+        </form>
+      </div>
+    </div>
+
+    <div class="card">
+      <div class="card-header">
+        <h3>Articles List</h3>
+      </div>
+      <div class="card-body">
+        <table class="table table-striped">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Author</th>
+              <th>Delete</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="article in articles" :key="article.id">
+              <td>
+                <a :href="article.url">{{ article.title }}</a>
+              </td>
+              <td>{{ article.author }}</td>
+              <td>
+                <span class="pointer"
+                      @click="removeArticle(article)">
+                  <i class="fas fa-trash"></i>
+                </span>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
+import articlesRef from './config'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  firebase: {
+    articles: articlesRef
+  },
+  data () {
+    return {
+      newArticle: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addArticle () {
+      articlesRef.push(this.newArticle)
+      this.newArticle.title = ''
+      this.newArticle.author = ''
+      this.newArticle.url = ''
+    },
+    removeArticle (article) {
+      articlesRef.child(article['.key']).remove()
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/config.js
+++ src/config.js
@@ -1,15 +1,17 @@
 // Initialize Firebase
-var config ={
-    apiKey: "AIzaSyCn0qavNizHEdoYfbcC7MXrvjWAroNpYrY",
-    authDomain: "vue-firebase-ae075.firebaseapp.com",
-    databaseURL: "https://vue-firebase-ae075.firebaseio.com",
-    projectId: "vue-firebase-ae075",
-    storageBucket: "",
-    messagingSenderId: "980067630024"
+import Firebase from 'firebase'
+
+var config = {
+  apiKey: 'AIzaSyCn0qavNizHEdoYfbcC7MXrvjWAroNpYrY',
+  authDomain: 'vue-firebase-ae075.firebaseapp.com',
+  databaseURL: 'https://vue-firebase-ae075.firebaseio.com',
+  projectId: 'vue-firebase-ae075',
+  storageBucket: '',
+  messagingSenderId: '980067630024'
 }
 
-let app = Firebase.initializeApp(config);
-let db = app.database();
-let articlesRef = db.ref('articles');
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let articlesRef = db.ref('articles')
 
-export default articlesRef;
+export default articlesRef
