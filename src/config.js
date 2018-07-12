216518ba4585d7dde53f82dccfc86db26fe5a3e0
Cindy <cindylay524@gmail.com>
2018-07-12 11:34:40 +0700

    Set up vue application to work with Firebase-- will work on part 2 next.

---
+++ src/config.js
@@ -0,0 +1,15 @@
+// Initialize Firebase
+var config ={
+    apiKey: "AIzaSyCn0qavNizHEdoYfbcC7MXrvjWAroNpYrY",
+    authDomain: "vue-firebase-ae075.firebaseapp.com",
+    databaseURL: "https://vue-firebase-ae075.firebaseio.com",
+    projectId: "vue-firebase-ae075",
+    storageBucket: "",
+    messagingSenderId: "980067630024"
+}
+
+let app = Firebase.initializeApp(config);
+let db = app.database();
+let articlesRef = db.ref('articles');
+
+export default articlesRef;
